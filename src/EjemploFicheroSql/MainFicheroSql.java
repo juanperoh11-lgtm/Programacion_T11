651a5d1824f3b6e438fc3960f67c2b683621cbf9
juanperoh11-lgtm. <juanperoh11@gmail.com>
2026-05-11 16:10:11 +0200

    Ejecutar un fichero

--- src/EjemploFicheroSql/MainFicheroSql.java
+++ src/EjemploFicheroSql/MainFicheroSql.java
@@ -1,9 +1,8 @@
 package EjemploFicheroSql;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,31 +10,28 @@ import java.sql.Statement;
 
 public class MainFicheroSql {
 	private static final String URL = "jdbc:mysql://localhost:3306/?allowMultiQueries=true";
-	
+
 	private static final String USUARIO = "root";
 
 	private static final String PASSWORD = ""; // vacía en XAMPP local
 
 	public static void main(String[] args) {
 
-		StringBuilder sb = new StringBuilder();
-		String todo = "";
+		String sqlScript;
 
 		try (Connection conn = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-			     Statement stm = conn.createStatement()) {
-			    
-			    // Dividimos el script en sentencias individuales
-			    String[] consultas = sb.toString().split(";");
-			    
-			    for (String consulta : consultas) {
-			        if (!consulta.trim().isEmpty()) {
-			            stm.execute(consulta.trim());
-			        }
-			    }
-			    System.out.println("¡Script ejecutado con éxito!");
+				Statement stm = conn.createStatement()) {
 
-			} catch (SQLException ex) {
-			    System.out.println("Error SQL: " + ex.getMessage());
-			}
+			sqlScript = Files.readString(Paths.get("inventario.sql"));
+
+			String[] sentencia = sqlScript.toString().split(";");
+			for (String s : sentencia)
+				stm.execute(s.trim());
+
+			System.out.println("¡Script ejecutado con éxito!");
+
+		} catch (SQLException | IOException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
